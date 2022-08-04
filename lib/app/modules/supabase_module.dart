821990349093a7ec6fdaa243ef8b12b0abb5cdcc
SandroMaglione <lass.maglio@gmail.com>
2022-08-04 19:45:43 +0200

    added supabase module dependency injection for Supabase instance

---
+++ lib/app/modules/supabase_module.dart
@@ -0,0 +1,8 @@
+import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+@module
+abstract class SupabaseModule {
+  @lazySingleton
+  Supabase supabase() => Supabase.instance;
+}
--- lib/app/services/supabase_auth_repository.dart
+++ lib/app/services/supabase_auth_repository.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_supabase_complete/app/repository/auth_repository.dart';
 import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 @Injectable(as: AuthRepository)
 class SupabaseAuthRepository implements AuthRepository {
+  final Supabase _supabase;
+  const SupabaseAuthRepository(this._supabase);
+
   @override
   Future<String> signIn() {
     // TODO: implement signIn
