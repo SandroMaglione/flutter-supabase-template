d3a0c0a0d3dd93fea03420968aa6b98e4935cd7b
SandroMaglione <lass.maglio@gmail.com>
2022-08-04 11:58:49 +0200

    get constants from environment

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,11 @@
-class Constants {
-  static const String supabaseUrl = "";
-  static const String supabaseAnnonKey = "";
+abstract class Constants {
+  static const String supabaseUrl = String.fromEnvironment(
+    'SUPABASE_URL',
+    defaultValue: 'SOME_DEFAULT_VALUE',
+  );
+
+  static const String supabaseAnnonKey = String.fromEnvironment(
+    'SUPABASE_ANNON_KEY',
+    defaultValue: 'SOME_DEFAULT_VALUE',
+  );
 }
