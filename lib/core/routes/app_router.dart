96cb446e2ba8a55cab5202b6b55a96147159830b
SandroMaglione <lass.maglio@gmail.com>
2022-08-04 18:18:36 +0200

    added setup auto route in App.dart

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,6 @@
-import 'package:auto_route/annotations.dart';
+/// Make sure to import `auto_route` and `material` (required)
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_supabase_complete/app/home_page.dart';
 
 part 'app_router.gr.dart';
