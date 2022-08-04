88e2b1b176f0e70213f82a6e0b24ae300547c6f1
SandroMaglione <lass.maglio@gmail.com>
2022-08-04 12:08:17 +0200

    added launch.json vscode config

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
 abstract class Constants {
   static const String supabaseUrl = String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: 'SOME_DEFAULT_VALUE',
+    defaultValue: '',
   );
 
   static const String supabaseAnnonKey = String.fromEnvironment(
     'SUPABASE_ANNON_KEY',
-    defaultValue: 'SOME_DEFAULT_VALUE',
+    defaultValue: '',
   );
 }
