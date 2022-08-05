bb6d2b9c7ac22319208c0aadf1ea731e020aa481
SandroMaglione <lass.maglio@gmail.com>
2022-08-05 18:17:52 +0200

    email and password text fields

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String email = "";
+  String password = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Email",
+              ),
+              onChanged: (value) {
+                setState(() => email = value);
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: "Password",
+              ),
+              onChanged: (value) {
+                setState(() => password = value);
+              },
+            ),
+            ElevatedButton(
+              onPressed: onClickSignUp,
+              child: const Text('Sign up'),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void onClickSignUp() {
+    print(email);
+    print(password);
   }
 }
