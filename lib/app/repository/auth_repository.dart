9d8970dfd19ad5a0aa4e7a657bd7fc8e1502bd22
SandroMaglione <lass.maglio@gmail.com>
2022-11-30 05:30:10 +0100

    update auth repository type signatures

--- lib/app/repository/auth_repository.dart
+++ lib/app/repository/auth_repository.dart
@@ -1,6 +1,18 @@
-abstract class AuthRepository {
-  Future<String> signInEmailAndPassword(String email, String password);
-  Future<String> signUpEmailAndPassword(String email, String password);
+import 'package:flutter_supabase_complete/app/failures/sign_in_failure.dart';
+import 'package:flutter_supabase_complete/app/failures/sign_out_failure.dart';
+import 'package:flutter_supabase_complete/app/failures/sign_up_failure.dart';
+import 'package:fpdart/fpdart.dart';
 
-  Future<void> signOut();
+abstract class AuthRepository {
+  TaskEither<SignInFailure, String> signInEmailAndPassword(
+    String email,
+    String password,
+  );
+
+  TaskEither<SignUpFailure, String> signUpEmailAndPassword(
+    String email,
+    String password,
+  );
+
+  TaskEither<SignOutFailure, Unit> signOut();
 }
