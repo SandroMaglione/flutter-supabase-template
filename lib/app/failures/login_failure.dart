aac4075bb5de665107bd5cd02a3423a68a6d458f
SandroMaglione <lass.maglio@gmail.com>
2022-11-30 06:11:55 +0100

    sign in page make request

--- lib/app/failures/login_failure.dart
+++ lib/app/failures/login_failure.dart
@@ -1,13 +1,24 @@
 abstract class LoginFailure {
   const LoginFailure();
+
+  String get mapToErrorMessage {
+    switch (runtimeType) {
+      case ExecutionErrorLoginFailure:
+        return 'Error when making sign in request';
+      case MissingUserIdLoginFailure:
+        return 'Missing user information';
+    }
+
+    return 'Unexpected error, please try again';
+  }
 }
 
-class ExecutionErrorLoginFailure implements LoginFailure {
+class ExecutionErrorLoginFailure extends LoginFailure {
   final Object error;
   final StackTrace stackTrace;
   const ExecutionErrorLoginFailure(this.error, this.stackTrace);
 }
 
-class MissingUserIdLoginFailure implements LoginFailure {
+class MissingUserIdLoginFailure extends LoginFailure {
   const MissingUserIdLoginFailure();
 }
